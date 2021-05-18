e6645ac9a2c40e011aaf17b02cfe0dde087b581c
absurdtales <hunter.barrett9@gmail.com>
2021-05-18 10:55:06 +1000

    Create program.py

---
+++ program.py
@@ -0,0 +1,23 @@
+import time as t
+import tkinter as tk
+
+def countdowntimer():
+
+    
+
+# --- Main Program ---
+# Create Window
+root = tk.Tk()
+root.title("Scoreboard Program")
+root.geometry("1920x1080")
+root.resizable = (False,False)
+
+# Configure
+root.config(bg = '#545f66')
+
+# Create elements
+
+
+# Global Variables
+
+root.mainloop()
