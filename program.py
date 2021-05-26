75f721daf684a55271c535d6d72f6470b0fbe48e
absurdtales <hunter.barrett9@gmail.com>
2021-05-26 14:13:36 +1000

    Update program.py

--- program.py
+++ program.py
@@ -59,7 +59,7 @@ root.current_time = root.time
 root.running = False
 
 # Configure
-root.config(bg = '#545f66')
+#root.config(bg = '#545f66')
 
 # Create elements
 # Frames
@@ -78,18 +78,18 @@ save_button_frame.config(bg = '#545f66')
 save_button_frame.grid(row=0, column=0, sticky="NW")
 
 # Left screen frames
-blue_team_frame = tk.Frame(left_frame, padx=50, pady=120, bg = "#00aeef", highlightbackground="black", highlightthickness=5)
+blue_team_frame = tk.Frame(left_frame, padx=50, pady=50, bg = "#00aeef", highlightbackground="black", highlightthickness=5)
 blue_team_frame.grid(row=0, column=1)
 
-blue_config_frame = tk.Frame(left_frame, padx=50, pady=20, bg = '#545f66')
+blue_config_frame = tk.Frame(left_frame, padx=50, pady=20)
 blue_config_frame.grid(row=1, column=1)
 
 # Right screen frames
-red_team_frame = tk.Frame(right_frame, padx=50, pady=120, bg="#ba014e", highlightbackground="black", highlightthickness=5)
+red_team_frame = tk.Frame(right_frame, padx=50, pady=50, bg="#ba014e", highlightbackground="black", highlightthickness=5)
 red_team_frame.grid(row=0, column=0)
 
-red_config_frame = tk.Frame(right_frame, padx=50, pady=20, bg = '#545f66')
-red_config_frame.grid(row=1, column=1)
+red_config_frame = tk.Frame(right_frame, padx=50, pady=20)
+red_config_frame.grid(row=1, column=0)
 
 # Image files/Resizing
 photo_1 = tk.PhotoImage(file = "/Users/hunterbarrett/Desktop/2021/Digital Solutions/Term 2 Work/mock ups gents/icons/noun_play_3923854.png")
@@ -105,19 +105,19 @@ photo_4 = tk.PhotoImage(file = "/Users/hunterbarrett/Desktop/2021/Digital Soluti
 photo_4_image = photo_4.subsample(15, 15)
 
 # Middle of the Screen
-time_lb = tk.Label(middle_frame,text=format_time(root.current_time),font=("Rockwell",150))
+time_lb = tk.Label(middle_frame,text=format_time(root.current_time),font=("Rockwell",200))
 time_lb.grid(row=0,column=0)
 
-start_btn = tk.Button(middle_frame,text="Start",font=("Rockwell",30),command=start, image = photo_1_image, compound=RIGHT)
+start_btn = tk.Button(middle_frame,text="Start",font=("Rockwell",50),command=start, image = photo_1_image, compound=RIGHT)
 start_btn.grid(row=1,column=0, sticky="WE", pady=10, padx=30)
 
-pause_btn = tk.Button(middle_frame,text="Pause",font=("Rockwell",30),command=pause, image = photo_2_image, compound = RIGHT)
+pause_btn = tk.Button(middle_frame,text="Pause",font=("Rockwell",50),command=pause, image = photo_2_image, compound = RIGHT)
 pause_btn.grid(row=2,column=0, sticky="WE", pady=10, padx=30)
 
-reset_btn = tk.Button(middle_frame,text="Reset",font=("Rockwell",30),command=reset, image = photo_3_image, compound = RIGHT)
+reset_btn = tk.Button(middle_frame,text="Reset",font=("Rockwell",50),command=reset, image = photo_3_image, compound = RIGHT)
 reset_btn.grid(row=3,column=0, sticky="WE", pady=10, padx=30)
 
-set_btn = tk.Button(middle_frame,text="Set",font=("Rockwell",30),command=set_time)
+set_btn = tk.Button(middle_frame,text="Set",font=("Rockwell",50),command=set_time)
 set_btn.grid(row=4,column=0,sticky="WE", pady=10, padx=30)
 
 # Corner Left of the Screen
@@ -128,35 +128,35 @@ load_btn = tk.Button(save_button_frame, text="Load", font=("Rockwell",12), padx=
 load_btn.grid(row=1, column=0)
 
 # Left of the Screen
-score_blue_lb = tk.Label(blue_team_frame, text="0", font=("Rockwell",250), bg="#00aeef", fg="white")
+score_blue_lb = tk.Label(blue_team_frame, text="0", font=("Rockwell",450), bg="#00aeef", fg="white")
 score_blue_lb.grid(row=0, column=0,sticky = "w")
 
-blue_config_btn_1 = tk.Button(blue_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10, bg = '#545f66')
+blue_config_btn_1 = tk.Button(blue_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10)
 blue_config_btn_1.grid(row=0, column=0)
 
-blue_config_btn_2 = tk.Button(blue_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10, bg = '#545f66')
+blue_config_btn_2 = tk.Button(blue_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10)
 blue_config_btn_2.grid(row=0, column=1)
 
-blue_config_btn_3 = tk.Button(blue_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10, bg = '#545f66')
+blue_config_btn_3 = tk.Button(blue_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10)
 blue_config_btn_3.grid(row=0, column=2)
 
-blue_config_btn_4 = tk.Button(blue_config_frame, text="Reset Score", font=("Rockwell", 20), padx=10, pady=10, bg = '#545f66')
+blue_config_btn_4 = tk.Button(blue_config_frame, text="Reset Score", font=("Rockwell", 20), padx=10, pady=10)
 blue_config_btn_4.grid(row=0, column=3)
 
 # Right of the Screen
-score_red_lb = tk.Label(red_team_frame, text="0", font =("Rockwell",250), bg="#ba014e", fg="white")
+score_red_lb = tk.Label(red_team_frame, text="0", font =("Rockwell",450), bg="#ba014e", fg="white")
 score_red_lb.grid(row=0, column=0, sticky="e")
 
-red_config_btn_1 = tk.Button(red_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10, bg = '#545f66')
+red_config_btn_1 = tk.Button(red_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10)
 red_config_btn_1.grid(row=0, column=0)
 
-red_config_btn_2 = tk.Button(red_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10, bg = '#545f66')
+red_config_btn_2 = tk.Button(red_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10)
 red_config_btn_2.grid(row=0, column=1)
 
-red_config_btn_3 = tk.Button(red_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10, bg = '#545f66')
+red_config_btn_3 = tk.Button(red_config_frame, text="+1", font=("Rockwell", 20), padx=10, pady=10)
 red_config_btn_3.grid(row=0, column=2)
 
-red_config_btn_4 = tk.Button(red_config_frame, text="Reset Score", font=("Rockwell", 20), padx=10, pady=10, bg = '#545f66')
+red_config_btn_4 = tk.Button(red_config_frame, text="Reset Score", font=("Rockwell", 20), padx=10, pady=10)
 red_config_btn_4.grid(row=0, column=3)
 
 
